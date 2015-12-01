5e874417809c02b0f1db6f90b38d8dd5c4cce7a8
tarena <tarena@niyoudu.local>
2015-12-01 10:06:19 +0800

    74554698468168/9796

---
+++ hhhuhuh/hhhuhuh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hhhuhuh
+//
+//  Created by tarena on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
